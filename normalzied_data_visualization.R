891b4794ed971385e3e219e5e1c7f2f6d1c53d49
hrmeredith12 <hrmeredith12@gmail.com>
2019-01-30 11:02:45 -0500

    Add files via upload
    This R script:
    - imports data that describes: origin of trip, destination of trip, number of trips made, and date. 
    - normalizes the number of trips for each destination by the total number of trips recorded on a given day. 
    - determines the number of "stays" for each location on a given date. 
    - manipulates the data into long form so it can be visualized with ggplot.

---
+++ normalzied_data_visualization.R
@@ -0,0 +1,83 @@
+## Visualizing mobility data
+## input: datafile with information: IDFrom, IDTo, Timestamp, Number of trips between 2 districts
+## by Hannah Meredith
+## Last updated January 30, 2019
+
+#  setwd("/Users/Hannah/Dropbox/Johns Hopkins/Mobility project/R")  # set working directory. Make sure both script and datafiles are in the same file
+#  install.packages("gdata") #
+#  install.packages("ggplot2")
+#  install.packages("reshape")
+# 
+# library(gdata) # library to import data
+# library(ggplot2)
+# library(reshape)
+
+mydata <- read.csv("NAM_between_day_mobility_updated.csv", check.names = FALSE) # import .cvs file; check.names is False because it was placing an X infront of date
+
+# determine total number of people at each timepoint for standardization
+
+totalTrips <- colSums(mydata[,-1:-2])
+normData<- mydata[,-1:-2]/totalTrips*100    # normalize raw data by the number of people who are reported to travel on a given date. Reported as a %
+
+
+origin <- mydata[,1]                        # ID of the district at origin of trip
+destination <-mydata[,2]                    # ID of the district at the destination of trip
+days <- seq(as.Date("2010-10-02"), as.Date("2014-04-30"), by = "days")
+#days_range <-seq(1,1307)   # numeric counter for time (days)
+
+# characteristics of dataset
+
+dimensions = dim(normData)
+pairs = dimensions[1]                          # number of possible pairs between origin and destination
+dates = dimensions[2]                          # dates traveled 
+
+districtNumber=unique(origin)                  # district number/ID  
+locations = length(districtNumber)             # number of districts
+
+
+# Stays
+counter <- 1 # counts through number of possible "stay" pairs
+stays <- matrix(ncol = dates, nrow = locations)  #establish empty matrix for loading
+
+for (d in 1:dates){           # if looking at number of stays for entire time window, then loop through all the dates 
+  counter <- 1                # reset counter to 1 for next date
+  for(p in 1:pairs){          # look at each entry to identify "stays", when both locations in a pair are the same
+    if (origin[p] == destination[p]){  # if a stay occured, then record the number of stays for that given location and date
+      stays[counter,d]= normData[p,d]  # add 2 to date index to account for i+j columns
+        if (counter < locations){
+        counter <- counter + 1  # update location counter
+      }
+    }
+  }
+}
+
+stays.df <- data.frame(stays, check.names=FALSE)   # convert matrix of stays into a dataframe
+colnames(stays.df) <- days
+
+stays.df <- cbind(districtNumber, stays.df)   #add column for District ID
+# colnames(stays.df[,-1]) <- days_range     # rename column names with date
+# rownames(stays.df) <- districtNumber
+stays_long <- melt(stays.df, id.vars = "districtNumber")   # melt matrix into long form
+stays_long$variable <- as.Date(stays_long$variable, format="%Y/%m/%d")  # convert date from factor to date form
+stays_long.df <- data.frame(stays_long)                         # convert into dataframe   
+colnames(stays_long.df)<- c("DistrictNumber", "Date", "Stays")  # name columns
+
+## create bar chart
+barplot(stays[,1],names.arg = districtNumber, xlab="locations", ylab="Normalized number of stays (%)") # plots a barchart looking at number of stays for each location on a given day
+barplot(stays[1,], xlab="date", ylab="Normalized number of stays (%)") # plots a barchart looking at number of stays over time for a given location
+
+# create line plot to compare all cities
+ggplot2(stays_long.df, aes(x=date,y))
+
+
+
+##John Giles' notes on parallel computing
+# 
+# library(parallel)
+# library(doParallel)
+# 
+# makeCluster(ncore=12) # register a parallel backend for the foreach loop
+# registerDoParallel()
+# 
+# x <- foreach(i=1:100, .combine='rbind') %:%
+#   foreach(j=1:100, .combine='c') %dopar% {}
